d178106a57d1a042a55209e294f30c2c7c801de4
Daniel García <116081500+fregonaespanola@users.noreply.github.com>
2024-06-05 00:37:35 +0200

    header and dashboard colors and icons

--- public/dashboard.php
+++ public/dashboard.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/styles_dashboard.css">
-
+    <link rel="stylesheet" href="css/styles_header.css">
     <style>
         body {
             background-color: #1f2937;
