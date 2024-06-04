f29d719e035f90a7b3413b3319b35a3b8b18733e
Daniel García <116081500+fregonaespanola@users.noreply.github.com>
2024-06-04 22:33:13 +0200

    form container with tw

--- public/js/form_container.js
+++ public/js/form_container.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-    // Función para cambiar entre las partes "Guion" y "Post-subida"
     $('#guion-tab').click(function() {
         $('#formulario-container').show();
         $('#formulario-checkboxes-container').hide();
-        $(this).addClass('active');
-        $('#post-subida-tab').removeClass('active');
+        $(this).addClass('border-bottom-red-vidmentor-secondary text-white');
+        $('#post-subida-tab').removeClass('border-bottom-red-vidmentor-secondary text-white');
     });
 
     $('#post-subida-tab').click(function() {
         $('#formulario-container').hide();
         $('#formulario-checkboxes-container').show();
-        $(this).addClass('active');
-        $('#guion-tab').removeClass('active');
+        $(this).addClass('border-bottom-red-vidmentor-secondary text-white');
+        $('#guion-tab').removeClass('border-bottom-red-vidmentor-secondary text-white');
     });
+
+    // Set default form to display
+    $('#guion-tab').click();
 });
