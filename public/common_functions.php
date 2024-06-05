2de648948fedfea25c41825aac9028ac68b270c2
Gyllenhaal <dalonsolaz@gmail.com>
2024-06-05 21:59:47 +0200

    actualizando la conexión a la DB

--- public/common_functions.php
+++ public/common_functions.php
@@ -1,70 +1,76 @@
 <?php
 
-function getDatabaseConnection() {
-    $dsn = "mysql:host=localhost;dbname=PROYECTO;charset=utf8mb4";
-    $username = "PROYECTO";
-    $password = "11223344";
+    use JetBrains\PhpStorm\NoReturn;
 
-    try {
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "Error al conectarse a la base de datos: " . $e->getMessage();
-        exit();
-    }
-}
+    require '../config.php';
 
-function executeQuery($query, $params = []) {
-    try {
-        $pdo = getDatabaseConnection();
-        $stmt = $pdo->prepare($query);
-        $stmt->execute($params);
-        return $stmt;
-    } catch (PDOException $e) {
-        // Manejo de errores
-        echo "Error en la consulta: " . $e->getMessage(); // Agregar mensaje de error
-        echo "<br>"; // Agregar salto de línea para una mejor legibilidad
-        echo "Query: " . $query; // Mostrar la consulta que causó el error
-        echo "<br>"; // Agregar salto de línea para una mejor legibilidad
-        echo "Params: " . print_r($params, true); // Mostrar los parámetros de la consulta
-        return false;
-    }
-}
+    function getDatabaseConnection() {
+        $dsn = "mysql:host=localhost;dbname=PROYECTO;charset=utf8mb4";
+        $username = "PROYECTO";
+        $password = DB_PASS;
 
-
-function checkExistingUserEmail($nick, $mail) {
-    $query = "SELECT ID FROM USUARIO WHERE NICK = :nick OR MAIL = :mail";
-    $params = [':nick' => $nick, ':mail' => $mail];
-    $stmt = executeQuery($query, $params);
-
-    if ($stmt && $stmt->rowCount() > 0) {
-        $data = $stmt->fetch(PDO::FETCH_ASSOC);
-        return ['exists' => true, 'data' => $data];
-    } else {
-        return ['exists' => false, 'data' => null];
-    }
-}
-
-function validateFields($requiredFields) {
-    $errors = [];
-    foreach ($requiredFields as $field => $displayName) {
-        if (empty($_POST[$field])) {
-            $errors[$field] = "El campo $displayName es obligatorio.";
+        try {
+            $pdo = new PDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "Error al conectarse a la base de datos: " . $e->getMessage();
+            exit();
         }
     }
-    return $errors;
-}
 
-function redirect($url, $msgType, $msg) {
-    $_SESSION[$msgType] = $msg;
-    header("Location: $url");
-    exit();
-}
+    function executeQuery($query, $params = []): false|PDOStatement
+    {
+        try {
+            $pdo = getDatabaseConnection();
+            $stmt = $pdo->prepare($query);
+            $stmt->execute($params);
+            return $stmt;
+        } catch (PDOException $e) {
+            // Manejo de errores
+            echo "Error en la consulta: " . $e->getMessage(); // Agregar mensaje de error
+            echo "<br>"; // Agregar salto de línea para una mejor legibilidad
+            echo "Query: " . $query; // Mostrar la consulta que causó el error
+            echo "<br>"; // Agregar salto de línea para una mejor legibilidad
+            echo "Params: " . print_r($params, true); // Mostrar los parámetros de la consulta
+            return false;
+        }
+    }
 
 
-function password_verify_custom($password, $hashedPassword) {
-    return password_verify($password, $hashedPassword);
-}
+    function checkExistingUserEmail($nick, $mail): array
+    {
+        $query = "SELECT ID FROM USUARIO WHERE NICK = :nick OR MAIL = :mail";
+        $params = [':nick' => $nick, ':mail' => $mail];
+        $stmt = executeQuery($query, $params);
 
-?>
+        if ($stmt && $stmt->rowCount() > 0) {
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            return ['exists' => true, 'data' => $data];
+        } else {
+            return ['exists' => false, 'data' => null];
+        }
+    }
+
+    function validateFields($requiredFields): array
+    {
+        $errors = [];
+        foreach ($requiredFields as $field => $displayName) {
+            if (empty($_POST[$field])) {
+                $errors[$field] = "El campo $displayName es obligatorio.";
+            }
+        }
+        return $errors;
+    }
+
+   function redirect($url, $msgType, $msg): void
+    {
+        $_SESSION[$msgType] = $msg;
+        header("Location: $url");
+        exit();
+    }
+
+    function password_verify_custom($password, $hashedPassword): bool
+    {
+        return password_verify($password, $hashedPassword);
+    }
